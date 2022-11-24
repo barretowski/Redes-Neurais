2cb6165aa616db9e3f232580875b53387e9c6025
Paulo Henrique <paulo.barreto@microton.com.br>
2022-11-23 21:42:43 -0300

    build table

---
+++ server/main.js
@@ -0,0 +1,50 @@
+import express from "express";
+import redesController from "./controllers/redesController.js";
+import cors from "cors";
+import multer from "multer";
+import Controller from "./controllers/redesController.js";
+import filePath from "path";
+import fs from "fs";
+
+const app = express();
+const controller = new Controller();
+app.use(express.json());
+app.use(cors());
+var header = new Vue();
+
+var storage = multer.diskStorage(
+    {
+        destination: './uploads/',
+        filename: function ( req, file, cb ) {
+            cb( null, file.originalname);
+        }
+    }
+);
+var upload = multer( { storage: storage } );
+
+
+app.post('/uploadFile', upload.array('file'), async (req, res) => {
+    controller.convertFileToJson(req.files[0].filename);
+    res.send({ upload: true, files: req.files });
+});
+app.get('/getJson', async (req,res) =>{
+    await controller.readJsonFile().then(
+        resp =>{
+            return res.json(resp);
+        }
+    ).catch(
+       err=>{
+        console.log(err);
+       }
+    );
+    
+})
+
+app.get("/",()=>{
+    
+})
+// executando o servidor
+app.listen(8081, ()=> {
+    console.log("Servidor na porta 8081");
+});
+
